435e7c9ea107103d2317a46445f0a707e33cf972
Andrew Knoesen <andrew@spatialedge.co.za>
2024-11-02 19:26:37 +0200

    Add the use of using IP address

--- virtual_business_card/src/App.jsx
+++ virtual_business_card/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import InstagramIcon from '@mui/icons-material/Instagram';
+import MenuIcon from '@mui/icons-material/Menu'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import './App.css'
 
 function App() {
@@ -10,7 +12,25 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank" className="logo-card">
           <InstagramIcon className="logo" fontSize="large" alt="Instagram Icon" />
-          Follow us on Instagram
+          Instagram
+        </a>
+      </div>
+      <div>
+        <a href="https://vite.dev" target="_blank" className="logo-card">
+          <WhatsAppIcon className="logo" fontSize="large" alt="WhatsApp Icon" />
+          WhatsApp
+        </a>
+      </div>
+      <div>
+        <a href="https://vite.dev" target="_blank" className="logo-card">
+          <MenuIcon className="logo" fontSize="large" alt="Menu Icon" />
+          Menu
+        </a>
+      </div>
+      <div>
+        <a href="https://vite.dev" target="_blank" className="logo-card">
+          <MenuIcon className="logo" fontSize="large" alt="Menu Icon" />
+          Menu
         </a>
       </div>
     </>
