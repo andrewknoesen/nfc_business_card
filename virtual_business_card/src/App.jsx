5c708c6ed9c8895988dd976d80a256a7e3d6e3cb
Andrew Knoesen <andrew@spatialedge.co.za>
2024-11-03 14:37:17 +0200

    Base design complete

--- virtual_business_card/src/App.jsx
+++ virtual_business_card/src/App.jsx
@@ -1,39 +1,14 @@
-import { useState } from 'react'
-import InstagramIcon from '@mui/icons-material/Instagram';
-import MenuIcon from '@mui/icons-material/Menu'
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import './App.css'
+import ContactCards from './assets/ContactCard/ContactCards';
+import ProfileCard from './assets/ProfileCard/ProfileCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" className="logo-card">
-          <InstagramIcon className="logo" fontSize="large" alt="Instagram Icon" />
-          Instagram
-        </a>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank" className="logo-card">
-          <WhatsAppIcon className="logo" fontSize="large" alt="WhatsApp Icon" />
-          WhatsApp
-        </a>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank" className="logo-card">
-          <MenuIcon className="logo" fontSize="large" alt="Menu Icon" />
-          Menu
-        </a>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank" className="logo-card">
-          <MenuIcon className="logo" fontSize="large" alt="Menu Icon" />
-          Menu
-        </a>
-      </div>
-    </>
+    <div style={{ width: "80vw", minHeight: "80vh" }}>
+      <ProfileCard />
+      <ContactCards />
+    </div>
   )
 }
 
